f569340b341a32cb1ca2111709caaa2803cfdc1a
adamajay <30343197+adamajay@users.noreply.github.com>
2017-07-23 10:05:34 +0530

    Create vc.c

---
+++ vc.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int ch;
+scanf("%d",&ch);
+if(ch==a||ch==e||ch==i||ch==o||ch==u)
+{
+printf("charc is a vowel");
+}
+else
+{
+printf("char is a consonent");
+}
+}
